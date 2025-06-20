266fe9e85204d1eff91c65a776f30faba6353fb8
lyay23 <3050681193@qq.com>
2025-06-20 16:31:31 +0800

    优化登录Bug，刷新会退出登录

--- his_ns_vue3-master/src/store/user.js
+++ his_ns_vue3-master/src/store/user.js
@@ -11,21 +11,27 @@ export const useUserStore=defineStore("user",()=>{
 		isAuth:false,
 	})
 
+	// 初始化时从 localStorage 恢复状态
+	const storedUser = localStorage.getItem("user");
+	if (storedUser) {
+		try {
+			const user = JSON.parse(storedUser);
+			if (user && user.token && user.isAuth) {
+				userInfo.value = user;
+			}
+		} catch (e) {
+			// 如果解析出错，清除损坏的数据
+			localStorage.removeItem("user");
+			localStorage.removeItem("token");
+			localStorage.removeItem("isAuth");
+		}
+	}
+	
 	const isLogin=ref(false)
 	
 	//computed() = getters
 	//返回用户登录信息
 	const getUserInfo = computed(()=>{
-		console.log("getUserInfo:start")
-		//pinia存储的用户信息和本地localStorage
-		console.log(userInfo.value.isAuth);
-		console.log(JSON.parse(localStorage.getItem("isAuth")));
-		if(userInfo.value.isAuth==false
-			&& JSON.parse(localStorage.getItem("isAuth")) ){
-			console.log("getUserInfo:更新用户信息")
-			setAuthenticated(JSON.parse(localStorage.getItem("user")))
-		}
-		console.log("getUserInfo:end")
 		return userInfo;
 	})
 
@@ -34,15 +40,11 @@ export const useUserStore=defineStore("user",()=>{
 	//function() = actions
 	//设置登录状态，获取登录信息
 	const setAuthenticated=(u)=>{
-		console.log("setAuthenticated:start")
 		userInfo.value=u;
 		//同步localStorage
-		console.log(u.useType)
 		localStorage.setItem("user",JSON.stringify(u));
 		localStorage.setItem("token",u.token);
 		localStorage.setItem("isAuth",u.isAuth);
-		console.log(`setAuthenticated:${userInfo.value.isAuth}`)
-		console.log("setAuthenticated:end")
 	}
 
 	// const setLoginState(f)=>{
@@ -60,16 +62,13 @@ export const useUserStore=defineStore("user",()=>{
 	}
 	//退出登录
 	const logOut=()=>{
-		console.log("logOut:start")
-		userInfo.value.isAuth=false
-		userInfo.value.token=''
+		userInfo.value = { id: "", token: "", isAuth: false };
 		//LocalStorage:除非主动删除，否则会永久存储在浏览器中。
-		localStorage.setItem("user",'');
-		localStorage.setItem("token",'');
-		localStorage.setItem("isAuth",false);
+		localStorage.removeItem("user");
+		localStorage.removeItem("token");
+		localStorage.removeItem("isAuth");
 		//SessionStorage:只在当前所在窗口关闭前有效，窗口关闭后其存储数据也就会被自动清除。
 		sessionStorage.clear()	
-		console.log("logOut:end")
 	}
 	
 	return {userInfo, isAuth, getUserInfo,setAuthenticated,logOut}
