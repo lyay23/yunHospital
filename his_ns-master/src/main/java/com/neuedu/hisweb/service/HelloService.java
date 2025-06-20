c1fbf57811bdeb080c27e88dcb1544fd2deb3399
lyay23 <3050681193@qq.com>
2025-06-20 08:50:05 +0800

    美化登录页面，去除了默认输入，让表单居中显示

--- his_ns-master/src/main/java/com/neuedu/hisweb/service/HelloService.java
+++ his_ns-master/src/main/java/com/neuedu/hisweb/service/HelloService.java
@@ -3,14 +3,6 @@ package com.neuedu.hisweb.service;
 import com.neuedu.hisweb.entity.Hello;
 import com.baomidou.mybatisplus.extension.service.IService;
 
-/**
- * <p>
- *  服务类
- * </p>
- *
- * @author xj
- * @since 2025-06-11
- */
-public interface HelloService extends IService<Hello> {
 
+public interface HelloService extends IService<Hello> {
 }
